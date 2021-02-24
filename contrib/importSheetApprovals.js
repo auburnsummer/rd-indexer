0f7d020326fc8b30ec0585bc23c6e48fb060e627
auburn <auburnete@gmail.com>
2021-02-24 10:45:41 +1000

    test

---
+++ contrib/importSheetApprovals.js
@@ -0,0 +1,70 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzm3I9ENulE7uOmze53cyDuj7Igi7fmGiQ6w045fCRxs_sK3D4/exec";
+
+const {default: axios} = require('axios');
+const { flatten } = require('lodash');
+
+(async () => {
+    const data = await axios({
+        method: "GET",
+        url: API_URL
+    })
+        .then( (resp) => {
+            return resp.data;
+        });
+    
+    const payload = data
+        .filter(level => level.verified != null)
+        .map(level => {
+            if (level.verified) {
+                return [
+                    {
+                        "op": "add",
+                        "path": "/-",
+                        "value": {
+                            "action": "approval",
+                            "set": 10,
+                            "s_id": "rdl",
+                            "s_iid": level.download_url
+                        }
+                    },
+                    {
+                        "op": "add",
+                        "path": "/-",
+                        "value": {
+                            "action": "approval",
+                            "set": 10,
+                            "s_id": "yeoldesheet",
+                            "s_iid": level.download_url
+                        }
+                    },
+                ]
+            } else {
+                return [
+                    {
+                        "op": "add",
+                        "path": "/-",
+                        "value": {
+                            "action": "approval",
+                            "set": -1,
+                            "s_id": "rdl",
+                            "s_iid": level.download_url
+                        }
+                    },
+                    {
+                        "op": "add",
+                        "path": "/-",
+                        "value": {
+                            "action": "approval",
+                            "set": -1,
+                            "s_id": "yeoldesheet",
+                            "s_iid": level.download_url
+                        }
+                    },
+                ]
+            }
+        });
+
+    const payload2 = flatten(payload);
+    console.log(payload2);
+
+})();
