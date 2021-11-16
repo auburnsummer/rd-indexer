500d657f740e384bcb567989f5108e5f2ba98424
auburn <auburnete@gmail.com>
2021-11-17 00:31:50 +1000

    wopehfpoahweopfahwopefopawehaopfahwopop

--- contrib/importSheetApprovals.js
+++ contrib/importSheetApprovals.js
@@ -1,10 +1,16 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzm3I9ENulE7uOmze53cyDuj7Igi7fmGiQ6w045fCRxs_sK3D4/exec";
-const BOT_API_URL = "https://bot2.auburn.dev"
+const BOT_API_URL = "http://20.106.135.138"
 
-const TOKEN = "SXQT97gWnUDv3CtlLYqUHgVA7NBqpGYjwKj3U8WIrwAc97EMKoU4B6JBCUvcETGWi1XHxkBC6W4PvRUomjtAcme89OWK9Qsc0/7YETRhTEDQ7YhY7XvLcUArMg==";
+const TOKEN = "tokenhere";
 
 const {default: axios} = require('axios');
 const { flatten, isArray } = require('lodash');
+
+const csv = require('csv-parse/sync');
+const fs = require('fs');
+
+const rawCsv = fs.readFileSync('./levels.csv', 'utf-8');
+const parsedCsv = csv.parse(rawCsv, {columns: true});
 
 (async () => {
     // get data...
@@ -19,9 +25,14 @@ const { flatten, isArray } = require('lodash');
     const approvedLevels = data.filter(r => r.verified);
     for (let level of approvedLevels) {
         console.log(level.download_url);
+        if (parsedCsv.find(r => r.url === level.download_url)) {
+            console.log("Already found a match!");
+            continue;
+        }
+
         const aaaa = await axios({
             method: "GET",
-            url: "https://api.rhythm.cafe/orchard/level.json",
+            url: "https://api.rhythm.cafe/orchard/levels.json",
             params: {
                 "_sort": "id",
                 "url__exact": level.download_url,
@@ -29,23 +40,23 @@ const { flatten, isArray } = require('lodash');
             }
         });
         if (isArray(aaaa.data) && aaaa.data.length > 0) {
-            // const resp = aaaa.data[0];
-            // console.log(`Found a match, the id is ${resp.id}`);
-            // if (resp.approval < 10) {
-            //     const bbbb = await axios({
-            //         method: 'POST',
-            //         url: `${BOT_API_URL}/approval/${resp.id}`,
-            //         data: {
-            //             approval: 10
-            //         },
-            //         headers: {
-            //             'authorization': `Bearer ${TOKEN}`
-            //         }
-            //     });
-            //     console.log(bbbb.data);
-            // } else {
-            //     console.log("already > 10, ignoring...");
-            // }
+            const resp = aaaa.data[0];
+            console.log(`Found a match, the id is ${resp.id} and the current approval is ${resp.approval}`);
+            if (resp.approval < 10) {
+                const bbbb = await axios({
+                    method: 'POST',
+                    url: `${BOT_API_URL}/approval/${resp.id}`,
+                    data: {
+                        approval: 10
+                    },
+                    headers: {
+                        'authorization': `Bearer ${TOKEN}`
+                    }
+                });
+                console.log(bbbb.data);
+            } else {
+                console.log("already > 10, ignoring...");
+            }
         }
 
     }
