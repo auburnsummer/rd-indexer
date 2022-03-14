dec0b31e4d1859e54182ba76dd46699d9085deeb
auburn <auburnete@gmail.com>
2022-03-14 10:58:51 +1000

    h9i9phiohiohiohiohiohiohiohiohoihiohwioehf-9wqh39-qhwf9-h90h-9

--- orchard/package/package.py
+++ orchard/package/package.py
@@ -3,9 +3,13 @@ import json
 import sys
 from math import floor
 
+import httpx
 from sqlite_utils import Database
 import itertools
+
+from orchard.bot.constants import DEFAULT_DB_VALUE
 from orchard.scan.schema import LEVEL_SCHEMA
+from orchard.scan.sources.old_sheet import SHEET_API_URL
 
 
 def make_json_from_row(row):
@@ -27,7 +31,37 @@ def make_json_from_row(row):
     return row
 
 
-def package(db: Database):
+def prerun_import_oldsheet(db: Database, status_db: Database):
+    resp = httpx.get(SHEET_API_URL, follow_redirects=True)
+    resp_json = resp.json()
+    approved_iids = [x['download_url'] for x in resp_json if 'verified' in x and x['verified']]
+    for iid in approved_iids:
+        q = list(db['level'].rows_where("source_iid = :iid", {'iid': iid}))
+        if q:
+            id = q[0]['id']
+            status_db['status'].upsert({
+                'id': id,
+                'approval': 10
+            }, pk='id')
+
+
+def package(db: Database, status_db: Database):
+    # db.attach("status", status_db)
+
+    prerun_import_oldsheet(db, status_db)
+
+    # map status id to the rest of the object.
+    statuses = { r['id']: {k: v for k, v in r.items() if k != 'id'} for r in status_db['status'].rows }
+    combined = []
+    for row in db['level'].rows:
+        id = row['id']
+        status = statuses[id] if id in statuses else {}
+        combined.append({
+            **row,
+            **DEFAULT_DB_VALUE,
+            **status
+        })
+
     with open("./orchard.jsonl", "w") as f:
         for row in db["level"].rows:
             processed = make_json_from_row(row)
@@ -37,4 +71,5 @@ def package(db: Database):
 
 if __name__ == "__main__":
     file = sys.argv[1]
-    package(Database(file))
+    status_file = sys.argv[2]
+    package(Database(file), Database(status_file))
