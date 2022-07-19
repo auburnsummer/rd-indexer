37592975c71c346abb2ca0de3022059acf00bad2
auburn <auburnete@gmail.com>
2022-07-19 14:56:46 +1000

    fawefawefawefawef

--- orchard/bot/handlers/status_db.py
+++ orchard/bot/handlers/status_db.py
@@ -8,16 +8,13 @@ from orchard.bot.constants import POST_STATUS_DOT_DB_TOKEN
 
 
 async def post_status_dot_db(request):
-    try:
-        if "authorization" not in request.headers:
-            raise ValueError("There should be an Authorization header, but there aint")
-        token_type, token = request.headers["authorization"].split(" ")
-        if token_type.lower() != "bearer":
-            raise ValueError("Token type should be Bearer.")
-        if token != POST_STATUS_DOT_DB_TOKEN:
-            raise ValueError("Token incorrect")
-    except Exception as e:
-        return JSONResponse({"error": str(e)}, 401)
+    if "authorization" not in request.headers:
+        raise ValueError("There should be an Authorization header, but there aint")
+    token_type, token = request.headers["authorization"].split(" ")
+    if token_type.lower() != "bearer":
+        raise ValueError("Token type should be Bearer.")
+    if token != POST_STATUS_DOT_DB_TOKEN:
+        raise ValueError("Token incorrect")
 
     # delete old db
     db = request.app.state.db
@@ -32,7 +29,10 @@ async def post_status_dot_db(request):
 
 async def status_dot_db(request):
     if request.method.lower() == 'post':
-        await post_status_dot_db(request)
+        try:
+            await post_status_dot_db(request)
+        except Exception as e:
+            return JSONResponse({"error": str(e)}, 401)
         return Response(status_code=204)
     else:
         return FileResponse(sys.argv[1], media_type="application/x-sqlite3")
