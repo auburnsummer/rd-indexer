54235adb122c56990bc1ec95455b3bf5c39a221e
Random Guy JCI <22722393+RandomGuyJCI@users.noreply.github.com>
2024-10-06 23:56:47 +0800

    Add the ability for the bot to react using the reactx command
    Pharmacists who want to ignore specific rdzips from a message should react with the number/s corresponding to the order of the attachment/s, then click the "Ignore rdzip attachments" command. The bot will then add its own number reaction to the message, and it will ignore the attachments when indexing. If no number reaction is seen, then the indexer will ignore all attachments in the message.

--- orchard/bot/handlers/interactions/commands/reactx.py
+++ orchard/bot/handlers/interactions/commands/reactx.py
@@ -11,15 +11,23 @@ async def _reactx(body, _):
     async with Interactor(body["token"]) as i:
         channel_id = body["channel_id"]
         token = body["token"]
+        user_id = body["member"]["user"]["id"]
         messages = body["data"]["resolved"]["messages"]
+        number_reactions = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
         for key, value in messages.items():
             message_id = value["id"]
+            for reaction in number_reactions:
+                if reaction not in [react["emoji"]["name"] for react in value["reactions"]]:
+                    continue
+                reaction_users = await i.get_reactions(channel_id, message_id, reaction)
+                if user_id in [reactor["id"] for reactor in reaction_users.json()]:
+                    await i.react(channel_id, message_id, reaction)
             await i.react(channel_id, message_id, "🚫")
         await i.edit(MessageBuilder().content("done."), "@original")
 
 
 reactx = SlashRoute(
-    name="reactx",
+    name="Ignore rdzip attachments",
     description="",
     handler=_reactx,
     default_permission=False,
